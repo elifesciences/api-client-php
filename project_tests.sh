062ffe4aa87a289f69d77bad3fa1c0c06ab5b882
Giorgio Sironi <g.sironi@elifesciences.org>
2016-06-20 12:52:25 +0000

    Adding PHPSpec XML log

--- project_tests.sh
+++ project_tests.sh
@@ -2,5 +2,5 @@
 set -e
 
 if [ "$dependencies" = "lowest" ]; then composer update --prefer-lowest --no-interaction; else composer update --no-interaction; fi;
-vendor/bin/phpspec run
+(vendor/bin/phpspec run --format=junit > build/phpspec.xml) && echo "PHPSpec tests passed - see build/phpspec.xml log"
 vendor/bin/phpunit
