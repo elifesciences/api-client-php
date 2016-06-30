2b31fd0495c3fe705e0cd40cf18e11393ef420ef
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-06-30 13:41:26 +0100

    Add subjects client (#7)

---
+++ spec/ApiClient/SubjectsClientSpec.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spec\eLife\ApiSdk\ApiClient;
+
+use eLife\ApiSdk\HttpClient;
+use eLife\ApiSdk\MediaType;
+use eLife\ApiSdk\Result\ArrayResult;
+use GuzzleHttp\Promise\FulfilledPromise;
+use GuzzleHttp\Psr7\Request;
+use PhpSpec\ObjectBehavior;
+
+final class SubjectsClientSpec extends ObjectBehavior
+{
+    private $httpClient;
+
+    public function let(HttpClient $httpClient)
+    {
+        $this->httpClient = $httpClient;
+
+        $this->beConstructedWith($httpClient);
+    }
+
+    public function it_gets_an_experiment()
+    {
+        $request = new Request('GET', 'subjects/cell-biology',
+            ['Accept' => 'application/vnd.elife.subject+json; version=2']);
+        $response = new FulfilledPromise(new ArrayResult(new MediaType('application/vnd.elife.subject+json',
+            2), ['foo' => ['bar', 'baz']]));
+
+        $this->httpClient->send($request)->willReturn($response);
+
+        $this->getSubject(2, 'cell-biology')->shouldBeLike($response);
+    }
+}
