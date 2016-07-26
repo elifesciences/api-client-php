91349756ef66eb472f75870c2016b9e4961fd7e6
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-07-26 10:55:05 +0100

    Add articles client (#21)

---
+++ src/ApiClient/ArticlesClient.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace eLife\ApiSdk\ApiClient;
+
+use eLife\ApiSdk\ApiClient;
+use GuzzleHttp\Promise\PromiseInterface;
+
+final class ArticlesClient
+{
+    const TYPE_ARTICLE_POA = 'application/vnd.elife.article-poa+json';
+    const TYPE_ARTICLE_VOR = 'application/vnd.elife.article-vor+json';
+    const TYPE_ARTICLE_LIST = 'application/vnd.elife.articles-list+json';
+    const TYPE_ARTICLE_HISTORY = 'application/vnd.elife.article-history+json';
+
+    use ApiClient;
+
+    public function getArticleLatestVersion(array $headers, string $number) : PromiseInterface
+    {
+        return $this->getRequest('articles/'.$number, $headers);
+    }
+
+    public function getArticleHistory(array $headers, string $number) : PromiseInterface
+    {
+        return $this->getRequest('articles/'.$number.'/versions', $headers);
+    }
+
+    public function getArticleVersion(array $headers, string $number, int $version) : PromiseInterface
+    {
+        return $this->getRequest('articles/'.$number.'/versions/'.$version, $headers);
+    }
+
+    public function listArticles(
+        array $headers = [],
+        int $page = 1,
+        int $perPage = 20,
+        bool $descendingOrder = true,
+        array $subjects = []
+    ) : PromiseInterface {
+        $subjectQuery = '';
+        foreach ($subjects as $subject) {
+            $subjectQuery .= '&subject[]='.$subject;
+        }
+
+        return $this->getRequest(
+            'articles?page='.$page.'&per-page='.$perPage.'&order='.($descendingOrder ? 'desc' : 'asc').$subjectQuery,
+            $headers
+        );
+    }
+}
