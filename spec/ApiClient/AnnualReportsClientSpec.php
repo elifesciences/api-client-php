8b01d69e2ba4b80ce4957fab14a35d2a1d3bb963
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-07-29 13:18:58 +0100

    ELPP-999 Add annual reports client (#24)
    * Add annual reports client
    
    * Add individual annual reports

---
+++ spec/ApiClient/AnnualReportsClientSpec.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace spec\eLife\ApiSdk\ApiClient;
+
+use eLife\ApiSdk\HttpClient;
+use eLife\ApiSdk\MediaType;
+use eLife\ApiSdk\Result\ArrayResult;
+use GuzzleHttp\Promise\FulfilledPromise;
+use GuzzleHttp\Psr7\Request;
+use PhpSpec\ObjectBehavior;
+
+final class AnnualReportsClientSpec extends ObjectBehavior
+{
+    private $httpClient;
+
+    public function let(HttpClient $httpClient)
+    {
+        $this->httpClient = $httpClient;
+
+        $this->beConstructedWith($httpClient, ['X-Foo' => 'bar']);
+    }
+
+    public function it_gets_an_annual_report()
+    {
+        $request = new Request('GET', 'annual-reports/2012',
+            ['X-Foo' => 'bar', 'Accept' => 'application/vnd.elife.annual-report+json; version=2']);
+        $response = new FulfilledPromise(new ArrayResult(new MediaType('application/vnd.elife.annual-report+json',
+            2), ['foo' => ['bar', 'baz']]));
+
+        $this->httpClient->send($request)->willReturn($response);
+
+        $this->getReport(['Accept' => 'application/vnd.elife.annual-report+json; version=2'], 2012)
+            ->shouldBeLike($response);
+    }
+
+    public function it_lists_annual_reports()
+    {
+        $request = new Request('GET', 'annual-reports?page=1&per-page=20&order=desc',
+            ['X-Foo' => 'bar', 'Accept' => 'application/vnd.elife.annual-report-list+json; version=2']);
+        $response = new FulfilledPromise(new ArrayResult(new MediaType('application/vnd.elife.annual-report-list+json',
+            2), ['foo' => ['bar', 'baz']]));
+
+        $this->httpClient->send($request)->willReturn($response);
+
+        $this->listReports(['Accept' => 'application/vnd.elife.annual-report-list+json; version=2'], 1, 20, true)
+            ->shouldBeLike($response);
+    }
+}
---
+++ src/ApiClient/AnnualReportsClient.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace eLife\ApiSdk\ApiClient;
+
+use eLife\ApiSdk\ApiClient;
+use GuzzleHttp\Promise\PromiseInterface;
+
+final class AnnualReportsClient
+{
+    const TYPE_ANNUAL_REPORT = 'application/vnd.elife.annual-report+json';
+    const TYPE_ANNUAL_REPORT_LIST = 'application/vnd.elife.annual-report-list+json';
+
+    use ApiClient;
+
+    public function getReport(array $headers, int $year) : PromiseInterface
+    {
+        return $this->getRequest('annual-reports/'.$year, $headers);
+    }
+
+    public function listReports(
+        array $headers = [],
+        int $page = 1,
+        int $perPage = 20,
+        bool $descendingOrder = true
+    ) : PromiseInterface {
+        return $this->getRequest(
+            'annual-reports?page='.$page.'&per-page='.$perPage.'&order='.($descendingOrder ? 'desc' : 'asc'),
+            $headers
+        );
+    }
+}
