506816b044dbc699c6b853801b899e991c8df5ca
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-08-15 11:45:34 +0100

    Add collections client

---
+++ src/ApiClient/CollectionsClient.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace eLife\ApiSdk\ApiClient;
+
+use eLife\ApiSdk\ApiClient;
+use GuzzleHttp\Promise\PromiseInterface;
+
+final class CollectionsClient
+{
+    const TYPE_COLLECTION = 'application/vnd.elife.collection+json';
+    const TYPE_COLLECTION_LIST = 'application/vnd.elife.collection-list+json';
+
+    use ApiClient;
+
+    public function getCollection(array $headers, string $id) : PromiseInterface
+    {
+        return $this->getRequest('collections/'.$id, $headers);
+    }
+
+    public function listCollections(
+        array $headers = [],
+        int $page = 1,
+        int $perPage = 20,
+        bool $descendingOrder = true,
+        string $subject = null
+    ) : PromiseInterface {
+        $subjectQuery = ('' !== trim($subject)) ? '&subject='.$subject : '';
+
+        return $this->getRequest(
+            'collections?page='.$page.'&per-page='.$perPage.'&order='.($descendingOrder ? 'desc' : 'asc').$subjectQuery,
+            $headers
+        );
+    }
+}
