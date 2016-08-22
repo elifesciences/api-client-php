2bedf3f5321526ea4ddc6f14e0b842d38be6b1e1
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-08-22 11:41:20 +0100

    Add interviews client (#28)

---
+++ src/ApiClient/InterviewsClient.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace eLife\ApiSdk\ApiClient;
+
+use eLife\ApiSdk\ApiClient;
+use GuzzleHttp\Promise\PromiseInterface;
+
+final class InterviewsClient
+{
+    const TYPE_INTERVIEW = 'application/vnd.elife.interview+json';
+    const TYPE_INTERVIEW_LIST = 'application/vnd.elife.interview-list+json';
+
+    use ApiClient;
+
+    public function getInterview(array $headers, string $id) : PromiseInterface
+    {
+        return $this->getRequest('interviews/'.$id, $headers);
+    }
+
+    public function listInterviews(
+        array $headers = [],
+        int $page = 1,
+        int $perPage = 20,
+        bool $descendingOrder = true
+    ) : PromiseInterface {
+        return $this->getRequest(
+            'interviews?page='.$page.'&per-page='.$perPage.'&order='.($descendingOrder ? 'desc' : 'asc'),
+            $headers
+        );
+    }
+}
