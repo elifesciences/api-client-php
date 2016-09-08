b0dbacbac285240b0c9029a665bcebb96087fdde
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-09-08 10:13:54 +0100

    Add Proofreader to CI

--- project_tests.sh
+++ project_tests.sh
@@ -2,6 +2,11 @@
 set -e
 
 : "${dependencies:?Need to set dependencies environment variable}"
-if [ "$dependencies" = "lowest" ]; then composer update --prefer-lowest --no-interaction; else composer update --no-interaction; fi;
+if [ "$dependencies" = "lowest" ]; then
+    composer update --prefer-lowest --no-interaction
+    proofreader spec/ src/ test/
+else
+    composer update --no-interaction
+fi
 (vendor/bin/phpspec run --format=junit | tee build/${dependencies}-phpspec.xml) && echo "PHPSpec tests passed - see build/phpspec.xml log"
 vendor/bin/phpunit --log-junit="build/${dependencies}-phpunit.xml"
