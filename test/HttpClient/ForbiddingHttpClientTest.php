1cbff78f85d1f161da08420993dc31af338ba763
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-14 09:28:35 +0000

    Using expectException*()

--- test/HttpClient/ForbiddingHttpClientTest.php
+++ test/HttpClient/ForbiddingHttpClientTest.php
@@ -3,19 +3,22 @@
 namespace eLife\ApiClient\HttpClient;
 
 use GuzzleHttp\Psr7\Request;
+use LogicException;
 use PHPUnit_Framework_TestCase;
 
 final class ForbiddingHttpClientTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @test
-     * @expectedException LogicException
-     * @expectedExceptionMessage Forbidden call: GET /foo HTTP/1.1
      */
     public function it_forbids_any_call()
     {
         $client = new ForbiddingHttpClient();
+        $request = new Request('GET', '/foo');
 
-        $client->send(new Request('GET', '/foo'));
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('Forbidden call: GET /foo HTTP/1.1');
+
+        $client->send($request);
     }
 }
