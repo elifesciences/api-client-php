ed37f673e6d4455c95b476bf1eea4e09c4e0c447
Chris Wilkinson <chriswilkinson84@gmail.com>
2017-02-03 08:41:44 +0000

    Add date ranges to covers and search. (#45)

--- spec/ApiClient/CoversClientSpec.php
+++ spec/ApiClient/CoversClientSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\eLife\ApiClient\ApiClient;
 
+use DateTimeImmutable;
 use eLife\ApiClient\HttpClient;
 use eLife\ApiClient\MediaType;
 use eLife\ApiClient\Result\ArrayResult;
@@ -23,14 +24,14 @@ final class CoversClientSpec extends ObjectBehavior
 
     public function it_lists_covers()
     {
-        $request = new Request('GET', 'covers?page=1&per-page=20&order=desc',
+        $request = new Request('GET', 'covers?page=1&per-page=20&order=desc&start-date=2017-01-02&end-date=2017-02-03',
             ['X-Foo' => 'bar', 'Accept' => 'application/vnd.elife.cover-list+json; version=2', 'User-Agent' => 'eLifeApiClient/'.Version::get()]);
         $response = new FulfilledPromise(new ArrayResult(new MediaType('application/vnd.elife.cover-list+json',
             2), ['foo' => ['bar', 'baz']]));
 
         $this->httpClient->send($request)->willReturn($response);
 
-        $this->listCovers(['Accept' => 'application/vnd.elife.cover-list+json; version=2'], 1, 20, true)->shouldBeLike($response);
+        $this->listCovers(['Accept' => 'application/vnd.elife.cover-list+json; version=2'], 1, 20, true, new DateTimeImmutable('2017-01-02'), new DateTimeImmutable('2017-02-03'))->shouldBeLike($response);
     }
 
     public function it_lists_current_covers()
--- src/ApiClient/CoversClient.php
+++ src/ApiClient/CoversClient.php
@@ -2,6 +2,7 @@
 
 namespace eLife\ApiClient\ApiClient;
 
+use DateTimeImmutable;
 use eLife\ApiClient\ApiClient;
 use GuzzleHttp\Promise\PromiseInterface;
 
@@ -15,10 +16,15 @@ final class CoversClient
         array $headers = [],
         int $page = 1,
         int $perPage = 20,
-        bool $descendingOrder = true
+        bool $descendingOrder = true,
+        DateTimeImmutable $starts = null,
+        DateTimeImmutable $ends = null
     ) : PromiseInterface {
+        $startsQuery = $starts ? '&start-date='.$starts->format('Y-m-d') : '';
+        $endsQuery = $ends ? '&end-date='.$ends->format('Y-m-d') : '';
+
         return $this->getRequest(
-            'covers?page='.$page.'&per-page='.$perPage.'&order='.($descendingOrder ? 'desc' : 'asc'),
+            'covers?page='.$page.'&per-page='.$perPage.'&order='.($descendingOrder ? 'desc' : 'asc').$startsQuery.$endsQuery,
             $headers
         );
     }
