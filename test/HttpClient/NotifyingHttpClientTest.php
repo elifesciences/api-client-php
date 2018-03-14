8648495b570cf04b731e6c8f421dfecb988094e5
Giorgio Sironi <g.sironi@elifesciences.org>
2018-03-14 17:21:10 +0000

    Proofreader 0.2 forward compatibility

--- test/HttpClient/NotifyingHttpClientTest.php
+++ test/HttpClient/NotifyingHttpClientTest.php
@@ -3,7 +3,6 @@
 namespace eLife\ApiClient\HttpClient;
 
 use eLife\ApiClient\HttpClient;
-use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit_Framework_TestCase;
