7e9203fd065c37cb8f268599a6072ed43cc47179
SaMoghadamzadeh <saeed.moghadamzadeh1@endava.com>
2022-08-10 14:07:11 +0200

    Add client for reviewed-preprint

---
+++ src/ApiClient/ReviewedPreprint.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace eLife\ApiClient\ApiClient;
+
+use eLife\ApiClient\ApiClient;
+use GuzzleHttp\Promise\PromiseInterface;
+
+final class ReviewedPreprint
+{
+    const TYPE_REVIEWED_PREPRINT = 'application/vnd.elife.reviewed-preprint-list+json';
+    const TYPE_REVIEWED_PREPRINT_LIST = 'application/vnd.elife.blog-article-list+json';
+
+    use ApiClient;
+
+    public function getReviewedPreprint(array $headers, string $id) : PromiseInterface
+    {
+        return $this->getRequest($this->createUri(['path' => "reviewed-preprint/$id"]), $headers);
+    }
+
+    public function listReviewedPreprint(
+        array $headers = [],
+        int $page = 1,
+        int $perPage = 20,
+        bool $descendingOrder = true
+    ) : PromiseInterface {
+        return $this->getRequest(
+            $this->createUri([
+                'path' => 'reviewed-preprint',
+                'query' => [
+                    'page' => $page,
+                    'per-page' => $perPage,
+                    'order' => $descendingOrder ? 'desc' : 'asc',
+                ],
+            ]),
+            $headers
+        );
+    }
+}
