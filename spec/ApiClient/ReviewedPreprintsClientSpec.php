42c424ea570a1d8c9eed977dd565cad84464de6f
Saeed Moqadam <smoqadam@users.noreply.github.com>
2022-08-18 20:50:00 +0200

    Add client for reviewed-preprint (#90)
    Co-authored-by: SaMoghadamzadeh <saeed.moghadamzadeh1@endava.com>
    Co-authored-by: nlisgo <nathan@aeopus.com>

---
+++ spec/ApiClient/ReviewedPreprintsClientSpec.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace spec\eLife\ApiClient\ApiClient;
+
+use eLife\ApiClient\HttpClient;
+use eLife\ApiClient\MediaType;
+use eLife\ApiClient\Result\ArrayResult;
+use eLife\ApiClient\Version;
+use GuzzleHttp\Promise\FulfilledPromise;
+use GuzzleHttp\Psr7\Request;
+use PhpSpec\ObjectBehavior;
+
+final class ReviewedPreprintsClientSpec extends ObjectBehavior
+{
+    private $httpClient;
+
+    public function let(HttpClient $httpClient)
+    {
+        $this->httpClient = $httpClient;
+
+        $this->beConstructedWith($httpClient, ['X-Foo' => 'bar']);
+    }
+
+    public function it_gets_a_reviewed_preprint()
+    {
+        $request = new Request('GET', 'reviewed-preprints/3',
+            ['X-Foo' => 'bar', 'Accept' => 'application/vnd.elife.reviewed-preprint+json; version=1', 'User-Agent' => 'eLifeApiClient/'.Version::get()]);
+        $response = new FulfilledPromise(new ArrayResult(new MediaType('application/vnd.elife.reviewed-preprint+json',
+            1), ['foo' => ['bar', 'baz']]));
+
+        $this->httpClient->send($request)->willReturn($response);
+
+        $this->getReviewedPreprint(['Accept' => 'application/vnd.elife.reviewed-preprint+json; version=1'], '3')
+            ->shouldBeLike($response)
+        ;
+    }
+
+    public function it_lists_reviewed_preprints()
+    {
+        $request = new Request('GET', 'reviewed-preprints?page=1&per-page=20&order=desc',
+            ['X-Foo' => 'bar', 'Accept' => 'application/vnd.elife.reviewed-preprint-list+json; version=1', 'User-Agent' => 'eLifeApiClient/'.Version::get()]);
+        $response = new FulfilledPromise(new ArrayResult(new MediaType('application/vnd.elife.reviewed-preprint-list+json',
+            1), ['foo' => ['bar', 'baz']]));
+
+        $this->httpClient->send($request)->willReturn($response);
+
+        $this->listReviewedPreprints(['Accept' => 'application/vnd.elife.reviewed-preprint-list+json; version=1'],
+            1, 20, true)->shouldBeLike($response)
+        ;
+    }
+}
---
+++ src/ApiClient/ReviewedPreprintsClient.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace eLife\ApiClient\ApiClient;
+
+use eLife\ApiClient\ApiClient;
+use GuzzleHttp\Promise\PromiseInterface;
+
+final class ReviewedPreprintsClient
+{
+    const TYPE_REVIEWED_PREPRINT = 'application/vnd.elife.reviewed-preprint+json';
+    const TYPE_REVIEWED_PREPRINT_LIST = 'application/vnd.elife.reviewed-preprint-list+json';
+
+    use ApiClient;
+
+    public function getReviewedPreprint(array $headers, string $id) : PromiseInterface
+    {
+        return $this->getRequest($this->createUri(['path' => "reviewed-preprints/$id"]), $headers);
+    }
+
+    public function listReviewedPreprints(
+        array $headers = [],
+        int $page = 1,
+        int $perPage = 20,
+        bool $descendingOrder = true
+    ) : PromiseInterface {
+        return $this->getRequest(
+            $this->createUri([
+                'path' => 'reviewed-preprints',
+                'query' => [
+                    'page' => $page,
+                    'per-page' => $perPage,
+                    'order' => $descendingOrder ? 'desc' : 'asc',
+                ],
+            ]),
+            $headers
+        );
+    }
+}
