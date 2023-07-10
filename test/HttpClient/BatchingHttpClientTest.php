e6cb18cf3c2de586d7b0a5fba7593ea342c2ec87
nlisgo <n.lisgo@elifesciences.org>
2023-07-10 19:56:56 +0100

    Revert "try this"
    This reverts commit fd965d123adaf67008907448a91afc90467bfbe0.

--- test/HttpClient/BatchingHttpClientTest.php
+++ test/HttpClient/BatchingHttpClientTest.php
@@ -6,9 +6,9 @@ use eLife\ApiClient\HttpClient;
 use GuzzleHttp\Promise\Promise;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
-use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_TestCase;
 
-class BatchingHttpClientTest extends TestCase
+class BatchingHttpClientTest extends PHPUnit_Framework_TestCase
 {
     public function testRandomSequenceOfSendAndWaits()
     {
