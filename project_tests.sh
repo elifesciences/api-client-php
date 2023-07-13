782e33d23d98a37e14167df52e3ebb3c7f6cc42f
Saeed Moghadamzadeh <saeed.moqadam@gmail.com>
2023-07-13 12:10:22 +0200

    fix github action pipeline

--- project_tests.sh
+++ project_tests.sh
@@ -3,10 +3,10 @@ set -e
 
 : "${dependencies:?Need to set dependencies environment variable}"
 if [ "$dependencies" = "lowest" ]; then
-    composer update --prefer-lowest --no-interaction
+    composer1 update --prefer-lowest --no-interaction
     vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p spec/ src/ test/
 else
-    composer update --no-interaction
+    composer1 update --no-interaction
 fi
 #vendor/bin/phpspec run
 vendor/bin/phpunit --log-junit="build/${dependencies}-phpunit.xml"
