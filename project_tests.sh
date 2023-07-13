f801789aa0ebf72ec90546e675b4a16f23f952d8
Saeed Moghadamzadeh <saeed.moqadam@gmail.com>
2023-07-13 12:11:21 +0200

    fix

--- project_tests.sh
+++ project_tests.sh
@@ -3,10 +3,10 @@ set -e
 
 : "${dependencies:?Need to set dependencies environment variable}"
 if [ "$dependencies" = "lowest" ]; then
-    composer1 update --prefer-lowest --no-interaction
+    composer update --prefer-lowest --no-interaction
     vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p spec/ src/ test/
 else
-    composer1 update --no-interaction
+    composer update --no-interaction
 fi
 #vendor/bin/phpspec run
 vendor/bin/phpunit --log-junit="build/${dependencies}-phpunit.xml"
